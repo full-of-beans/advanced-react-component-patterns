e32ee15403b008b67e8982f5fa461f6314369262
iamoperand <narora200@gmail.com>
2018-11-11 17:10:18 +0530

    [Feat]: Added links to all the exercises in the deployment

--- src/app.js
+++ src/app.js
@@ -5,17 +5,17 @@ import loadable from 'react-loadable'
 
 const files = [
   '01',
-  // '02',
-  // '03',
-  // '04',
-  // '05',
-  // '06',
-  // '07',
-  // '08',
-  // '09',
-  // '10',
-  // '11',
-  // '12',
+  '02',
+  '03',
+  '04',
+  '05',
+  '06',
+  '07',
+  '08',
+  '09',
+  '10',
+  '11',
+  '12',
 ]
 
 const pages = files.reduce((p, filename, index, fullArray) => {
